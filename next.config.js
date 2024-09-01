504f1b9ef779c13497446e7fcaf50c59b0e0339e
pizzou <pmpumuropizzou@gmail.com>
2024-09-01 02:42:48 +0200

    second commit

--- next.config.js
+++ next.config.js
@@ -1,28 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify:true,
-    async headers(){
-      return [
-        {
-          source:'/api/v1/:path*',
-          headers:[
-            {
-              key:'Access-Control-Allow-Credentials', value:'true' 
-            },
-            {
-              key:'Access-Control-Allow-Origin', value:'*'
-            },
-            {
-              key:'Access-Control-Allow-Methods',
-              value:'GET, POST,PUT,DELETE,OPTIONS,PATCH',
-            },
-            {
-              key:'Access-Control-Allow-Headers',
-              value:'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-            },
-          ],
-        },
-      ];
-    },
-  };
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://admin-dashboard-gvbn.onrender.com/api/:path*',
+      },
+    ];
+  },
+};
