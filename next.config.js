226b5c833c9230d33b930a151d321e655b8d7b60
pizzou <pmpumuropizzou@gmail.com>
2024-09-02 09:22:02 +0200

    third commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,36 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://admin-dashboard-gvbn.onrender.com/api/:path*',
+const nextConfig = {
+    images: {
+        domains: ['res.cloudinary.com','randomuser.me'],
       },
-    ];
-  },
-};
+      async headers() {
+        return [
+          {
+            // Routes this applies to
+            source: "/api/(.*)",
+            // Headers
+            headers: [
+              // Allow for specific domains to have access or * for all
+              {
+                key: "Access-Control-Allow-Origin",
+                value: "https://admin-dashboard-gvbn.onrender.com/api/v1",
+                // DOES NOT WORK
+                // value: process.env.ALLOWED_ORIGIN,
+              },
+              // Allows for specific methods accepted
+              {
+                key: "Access-Control-Allow-Methods",
+                value: "GET, POST, PUT, DELETE, OPTIONS",
+              },
+              // Allows for specific headers accepted (These are a few standard ones)
+              {
+                key: "Access-Control-Allow-Headers",
+                value: "Content-Type, Authorization",
+              },
+            ],
+          },
+        ];
+      },
+    };
+    
+    module.exports = nextConfig;
